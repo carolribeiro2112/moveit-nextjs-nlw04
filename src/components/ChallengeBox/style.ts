643989f4ff3c56cd4ff38d6da333b007dc4c15d5
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2021-10-20 10:50:36 -0300

    challenge box styles

--- src/components/ChallengeBox/style.ts
+++ src/components/ChallengeBox/style.ts
@@ -35,9 +35,94 @@ export const ChallengeNotActive = styled.div`
     max-width: 70%;
     margin-top: 3rem;
 
-    svg {
+    img {
       margin-bottom: 1rem;
     }
   }
+`;
 
+export const ChallengeIsActive = styled.div`
+  height: 100%; 
+
+  display: flex;
+  flex-direction: column;
+
+  header {
+    color: var(--blue);
+    font-weight: 600;
+    font-size: 1.25rem;
+    padding: 0 2rem 1.5rem;
+    border-bottom: 1px solid var(--gray-line);
+  }
+
+  main {
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    strong {
+      font-size: 2rem;
+      font-weight: 600;
+      color: var(--title);
+      margin: 1.5rem 0 1rem;
+    }
+
+    p {
+      line-height: 1.5;
+    }
+  }
+
+  footer {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 1rem;
+  }
+`;
+
+export const FailedButton = styled.button`
+   height: 3rem;
+
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   
+   border: 0;
+   border-radius: 5px;
+
+   color: var(--white);
+   background-color: var(--red);
+
+   font-size: 1rem;
+   font-weight: 600;
+
+   transition: filter 0.2s;
+
+   &:hover {
+     filter: brightness(0.9);
+   }
+`;
+
+export const CompletedButton = styled.button`
+  height: 3rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  border: 0;
+  border-radius: 5px;
+
+  color: var(--white);
+  background-color: var(--green);
+
+  font-size: 1rem;
+  font-weight: 600;
+
+  transition: filter 0.2s;
+
+  &:hover {
+  filter: brightness(0.9);
+  }
 `;
